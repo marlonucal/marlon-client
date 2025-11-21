7cf45dfa850af78a5368a7ffdf683061f2f9df01
Blaga Razvan <161320740+BlagaRa@users.noreply.github.com>
2025-11-21 12:17:48 +0200

    Remove proxy configuration from vite.config.js
    Removed proxy configuration from server settings.

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5173,
-    proxy: {
-      // în dev, poți lăsa /api relativ și folosești proxy -> Render
-      "/api": {
-        target: "https://marlon-app-2.onrender.com",
-        changeOrigin: true,
-        secure: true
-      }
-    }
+    port: 5173
   }
 });
